17e36c62850b12582dc3615f3159fb5c624f6c32
alessandrob17 <a.baldipardi@gmail.com>
2023-02-27 15:32:31 +0100

    FEAT done

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 const { createApp } = Vue
+
+createApp({
+    data() {
+      return {
+        
+        emails: [],
+
+      }
+    },
+    methods: {
+        fetchEmails() {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {
+                this.emails.push(res.data.response)
+            })
+        },
+
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            this.fetchEmails
+        }
+    }
+  }).mount('#app')
